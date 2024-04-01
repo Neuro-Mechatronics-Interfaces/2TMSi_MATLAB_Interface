9a322f99cb06760bdf168999d4c655d355ce9e47
Max (Lenovo) <max.murphy11@gmail.com>
2024-04-01 10:31:31 -0400

    Add function for exporting preliminary muap templates/analysis

--- example_decomposition.m
+++ example_decomposition.m
@@ -1,18 +1,24 @@
 %EXAMPLE_DECOMPOSITION Shows how to do decomposition of MUAPs
+clear;
+close all force;
+clc;
 
 % Set constants here
-K = 12;
+K = 32;
+NPC = 6;
+CH = 28;
 DATA_FILE = 'Max_2024_03_30_B_22.poly5';
 LAMBDA = 0.1;
 GAMMA = 1;
 
 %% Load data and run spike detection
 data = TMSiSAGA.Poly5.read(DATA_FILE);
-[S,uni_d] = uni_2_pks(data.samples(2:65,:));
-
-%% Decompose the sparse matrix
-W0 = pca(uni_d,'NumComponents',K);
-W0(W0 < 0) = 0;
+uni = data.samples(2:65,:);
+[S,uni_d] = uni_2_pks(uni);
+idx = find(S(CH,:));
+[snips,idx] = uni_2_extended(uni, idx);
+Y = tsne(snips);
+clus = kmeans(Y,K);
 
 %% Run the objective minimization
 % Define the objective function
