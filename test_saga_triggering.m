a8d0b1e02d6e8c969b313123f9d85c8d1fd161b3
Max (Lenovo) <max.murphy11@gmail.com>
2024-12-27 09:30:52 -0500

    Updates polling script, adds triggers script, new typewriter version

--- test_saga_triggering.m
+++ test_saga_triggering.m
@@ -1,4 +1,4 @@
-%TEST_SAGA_POLLING Test script to check on polling synchronously from 2 TMSi-SAGA + modified output to single poly5 binary along with data from 8BitDo Joystick (cursor.Cursor).
+%TEST_SAGA_TRIGGERING Test script to check on polling synchronously from 2 TMSi-SAGA + converting SAGA polling into emulated control commands.
 clc;
 
 if exist('device', 'var')~=0
@@ -16,9 +16,6 @@ else
     clear all;
 end
 
-lib = TMSiSAGA.Library();
-device = lib.getDevices('usb', 'electrical', 2, 2);
-connect(device);
 
 %% Name output files
 SUBJ = "Max";
@@ -27,43 +24,47 @@ MAX_TIME_SECONDS = 120; % Acquisition will not last longer than this (please onl
 BATCH_SIZE_SECONDS = 0.010; % Each acquisition cycle grabs sample batches of this duration (sample count depends on sample rate).
 BETA = getfield(load('Default_PLS_Coefficients_RH.mat','beta'),'beta');
 GAIN = 1;
-USE_JOYSTICK = false;
+TRIGGER_BIT = 8;
+BIT_MASK = 2^TRIGGER_BIT;
 TEENSY_PORT = "COM6"; % REQUIRED to have Teensy plugged in on USB COM port!
 
 dt = datetime();
 s = sprintf('%s_%04d_%02d_%02d',SUBJ,year(dt), month(dt), day(dt));
 
-%% Create poly5 files to write to
+%% Open library and connect to device
+
+lib = TMSiSAGA.Library();
+device = lib.getDevices('usb', 'electrical', 2, 2);
+connect(device);
+
+%% Configure devices and channels
 config_file = parameters('config_stream_service_plus');
 fprintf(1, "[TMSi]::Loading configuration file (%s, in main repo folder)...\n", config_file);
-[config, TAG, SN, N_CLIENT] = parse_main_config(config_file);
+[config, TAG, SN, N_CLIENT, SAGA, config_device, config_channels] = parse_main_config(config_file);
 setDeviceTag(device, SN, TAG);
-
+en_ch = horzcat({device.channels});
+enableChannels(device, en_ch);
+channelOrder = textile_8x8_uni2grid_mapping();
+if strcmpi(device(1).tag, "B") % Always puts "B" channels second (in online part)
+    channelOrder = [channelOrder+numel(device(1).getActiveChannels()), channelOrder];
+    iTrigger =find(device(2).getActiveChannels().isTrigger,1,'first') + numel(device(1).getActiveChannels()); % Get TRIGGERS from "A"
+else
+    channelOrder = [channelOrder, channelOrder+numel(device(1).getActiveChannels())];
+    iTrigger = find(device(1).getActiveChannels().isTrigger,1,'first'); % Get TRIGGERS from "A"
+end
+for ii = 1:numel(device)
+    setSAGA(device(ii).channels, device(ii).tag);
+    configStandardMode(device(ii), config_channels.(device(ii).tag), config_device);
+    fprintf(1,'\t->\tDetected device(%d): SAGA=%s | API=%d | INTERFACE=%s\n', ...
+        ii, device(ii).tag, device(ii).api_version, device(ii).data_recorder.interface_type);
+end
 ch = device.getActiveChannels();
 all_ch = channels_cell_2_sync_channels(ch);
 all_ch = add_channel_struct(all_ch, 'BatchTS', 'sec', "T", -1);
-
-if USE_JOYSTICK
-    all_ch = add_channel_struct(all_ch, 'JoyPx', '-', "X", -1); %#ok<*UNRCH>
-    all_ch = add_channel_struct(all_ch, 'JoyPy', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'JoyVx', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'JoyVy', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'JoyAx', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'JoyAy', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'JoyBtn','-', "X", -1);
-    
-    all_ch = add_channel_struct(all_ch, 'Target','-', "G", -1);
-    all_ch = add_channel_struct(all_ch, 'Score', '-', "G", -1);
-    all_ch = add_channel_struct(all_ch, 'Trial', '-', "G", -1);
-    
-    all_ch = add_channel_struct(all_ch, 'PredPx', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'PredPy', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'PredVx', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'PredVy', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'PredAx', '-', "X", -1);
-    all_ch = add_channel_struct(all_ch, 'PredAy', '-', "X", -1);
-end
 fs = double(device(1).sample_rate); % Should both be the same sample rate
+
+
+%% Initialize filter and state buffers
 batch_samples = fs * BATCH_SIZE_SECONDS;
 ticks_per_second = round(1/BATCH_SIZE_SECONDS);
 max_clock_cycles = ticks_per_second * MAX_TIME_SECONDS;
@@ -73,10 +74,13 @@ z_hpf = zeros(3,128);
 z_env = zeros(1,128);
 [b_p,a_p] = butter(1,6/(fs/2),'low');
 z_p = zeros(1,128);
-channelOrder = textile_8x8_uni2grid_mapping();
-channelOrder = [channelOrder+70, channelOrder];
 
-%%
+%% Create tcpclient which will transmit the commands
+conn = tcpclient("127.0.0.1", 6053);
+
+
+%% Create poly5 files to write to
+buttonState = false;
 mainTic = tic();
 POLY5_OUTPUT_FILE = string(sprintf("%s_Synchronized_%d.poly5", s, BLOCK));
 p5 = TMSiSAGA.Poly5(POLY5_OUTPUT_FILE, fs, all_ch, 'w');
@@ -92,11 +96,6 @@ fprintf(1,'device(2) starting COUNTER sample: %d\n', i_start(2));
 % there are in each array.
 iCount = 1;
 
-if USE_JOYSTICK
-    cObj = cursor.Cursor('BufferSamples', batch_samples, 'MainTick', mainTic);
-    cObj.setLogging(true, strrep(POLY5_OUTPUT_FILE, ".poly5", ".dat"));
-end
-
 startTick = datetime();
 averageTime = 0;
 
@@ -107,18 +106,25 @@ while isvalid(fig)
     uni_s = reshape(del2(reshape(uni,8,16,[])),128,[]);
     [uni_e,z_env] = filter(b_env,a_env,abs(uni_s),z_env,2);
     combined_data = [data; ones(1, batch_samples).*batch_toc];
-    if USE_JOYSTICK
-        state = [ones(batch_samples,1), uni_e'] * BETA * GAIN;
-        combined_data = [combined_data; repelem(cObj.StateBuffer(:,(end-3):end),1,5); state'];
-    end
     p5.append(combined_data);
+    if buttonState % If button is pressed, check if we should un-press it.
+        if any(bitand(data(iTrigger,:),BIT_MASK)==0)
+            writeline(conn, 'a1'); % Release the button
+            buttonState = false;
+        end
+    else
+        if any(bitand(data(iTrigger,:),BIT_MASK)==BIT_MASK)
+            writeline(conn, 'a0'); % Press the button
+            buttonState = true;
+        end
+    end
     curTick = datetime();
     delta_t = seconds(curTick - startTick);
     averageTime = ((iCount-1)*averageTime + delta_t)/iCount; 
     if rem(iCount,ticks_per_second)==0 % Roughly once per second, print debug info
-        cmd = sprintf('%d',randi(5,1)-1);
+        % cmd = sprintf('%d',randi(5,1)-1);
         try %#ok<TRYNC>
-            teensy.write(cmd,'char');
+            teensy.write('1','char');
         end
         fprintf(1,'Average loop: %0.3f s\n', round(averageTime,2));
         if iCount == max_clock_cycles
@@ -126,14 +132,8 @@ while isvalid(fig)
             break;
         end
     end
-    if USE_JOYSTICK
-        cObj.update(mean(state(:,3),1), mean(state(:,4),1), delta_t);
-    end
     iCount = iCount + 1;
     startTick = curTick;
-end
-if USE_JOYSTICK
-    delete(cObj);
 end
 delete(teensy);
 delete(fig);
