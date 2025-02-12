9e699d8723f0b3c74b26632dd834617f701e331d
Max (Lenovo) <max.murphy11@gmail.com>
2025-02-12 11:43:28 -0500

    Add script for mcp05 online

---
+++ mcp05_2025_02_12_train_pls_model.m
@@ -0,0 +1,17 @@
+
+SUBJ = "MCP05";
+YYYY = 2025;
+MM = 2;
+DD = 12;
+
+% These are specific to the ordering of gestures ("TRIAL")
+BLOCK = (1:6)';
+ENCODING = [0 1 0; ... % Left-handed wrist ext
+            0 0 1; ... % Left-handed ring flex
+            0 -1 0; ... % Left-handed wrist flex
+            -1 0 0; ... % Left-handed ulnar dev
+            1 0 0; ... % Left-handed radial dev
+            0 0.25 0.5; ... % Left-handed thumb ext
+            ];
+
+[XL,YL,XS,YS,BETA,PCTVAR,MSE,stats] = train_cosmo_decoder(SUBJ,YYYY,MM,DD,BLOCK,ENCODING);
