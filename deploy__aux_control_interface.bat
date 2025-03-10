a133e3d6d7962bc4192277478ba138e5ae3c9248
NML-Wean <max.murphy11@gmail.com>
2025-03-10 15:37:45 -0400

    From Scaife TMS desktop rig

---
+++ deploy__aux_control_interface.bat
@@ -0,0 +1,2 @@
+matlab -nosplash -nodesktop -batch "deploy__aux_control_interface; exit"
+exit
---
+++ deploy__aux_control_interface.m
@@ -0,0 +1,5 @@
+%DEPLOY__AUX_CONTROL_INTERFACE  Deploys GUI for sending UDP messages to SAGA device handler state machine.
+fig = init_aux_controller_fig();
+waitfor(fig);
+disp("[AUX-CONTROLLER]::SAGA AUX-Controller GUI exited.");
+pause(1);
